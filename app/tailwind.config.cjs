ff45726b9e772b9029c2103bbf7c1d58132c2be7
indexpro24@gmail.com <indexpro24@gmail.com>
2022-11-15 21:46:14 +0300

    с

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		'./src/**/*.{html,js,svelte,ts}',
-		'./pages/**/*.{html,js}',
-		'./components/**/*.{html,js}'
-	],
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
