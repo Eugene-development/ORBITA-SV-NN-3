e52e1b6003765c571ce956e175a850d8a31d1429
indexpro24@gmail.com <indexpro24@gmail.com>
2022-12-06 18:07:41 +0300

    с

--- app/src/lib/components/article/index.svelte
+++ app/src/lib/components/article/index.svelte
@@ -6,109 +6,125 @@
 				class="text-3xl font-extrabold tracking-tight text-white sm:text-4xl"
 			>
 				<span class="mb-6 block text-5xl text-indigo-400">"Орбита-Строй"</span>
-				<span class="block text-white">это база строительных и отделочных материалов</span>
-				<span class="block text-gray-100">в Дзержинске</span>
+				<span class="block text-white">это интернет база строительных и отделочных материалов</span>
+				<span class="block text-gray-100">в Нижнем Новгороде</span>
 			</h2>
 			<p class="mt-3 text-lg text-gray-300">
 				Осуществляем оптовую и розничную продажу строительных и отделочных материалов со складов в
-				Дзержинске. Широкий ассортимент и доступные цены приятно вас удивят. При необходимости есть
+				Нижнем Новгороде через наш интернет-магазин. Широкий ассортимент и доступные цены приятно вас удивят. При необходимости есть
 				услуга доставки стройматериалов до вашего объекта.
 			</p>
 		</div>
 	</div>
 </div>
 
-<div class="overflow-hidden bg-white">
-	<div class="relative mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
-		<div class="absolute top-0 bottom-0 left-3/4 hidden w-screen bg-gray-50 lg:block" />
-		<div class="mx-auto max-w-prose text-base lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-8">
-			<div>
-				<h2 class="text-base font-semibold uppercase tracking-wide text-indigo-600">
-					Строительная база
-				</h2>
-				<h3 class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-					Орбита-строй
-				</h3>
-			</div>
-		</div>
-		<div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
-			<div class="relative lg:col-start-2 lg:row-start-1">
-				<svg
-					class="absolute top-0 right-0 -mt-20 -mr-20 hidden lg:block"
-					width="404"
-					height="384"
-					fill="none"
-					viewBox="0 0 404 384"
-					aria-hidden="true"
-				>
-					<defs>
-						<pattern
-							id="de316486-4a29-4312-bdfc-fbce2132a2c1"
-							x="0"
-							y="0"
-							width="20"
-							height="20"
-							patternUnits="userSpaceOnUse"
-						>
-							<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
-						</pattern>
-					</defs>
-					<rect width="404" height="384" fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
-				</svg>
-				<div class="relative mx-auto max-w-prose text-base lg:max-w-none">
-					<figure>
-						<div class="aspect-w-12 aspect-h-7 lg:aspect-none">
-							<img
-								class="rounded-lg object-cover object-center shadow-lg"
-								src="https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/pages/serv_4.jpg"
-								alt="stroymat"
-								width="1184"
-								height="1376"
-							/>
-						</div>
-					</figure>
-				</div>
-			</div>
-			<div class="mt-8 lg:mt-0">
-				<div class="mx-auto max-w-prose text-base lg:max-w-none">
-					<p
-						class="prose prose-indigo mx-auto mt-1 text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none"
-					>
-						Компания «Орбита Строй» – это не просто стройбаза, это надежный партнер, сотрудничающий
-						с организациями, строительно-ремонтными бригадами, частными лицами и розничными
-						продавцами! Клиенты приобретают стройматериалы в Дзержинске с доставкой до объекта или
-						на условиях самовывоза.
-					</p>
-				</div>
-				<div
-					class="prose prose-indigo mx-auto mt-1 text-gray-500 lg:col-start-1 lg:row-start-1 lg:max-w-none"
-				>
-					<p>
-						В перечне товаров базы «Орбита Строй» представлено все необходимое для комплектации
-						ассортимента строительного магазина или оснащения бригады рабочих для выполнения
-						профессиональных задач. Клиентам предоставляется возможность заказать инструмент,
-						сантехнику, изоляцию, электротовары, строительные и отделочные материалы по выгодным
-						ценам. Покупателям товаров на строительной базе «Орбита Строй» доступны различные акции
-						лояльности, позволяющие значительно сэкономить на приобретении стройматериалов и
-						инструментов.
-					</p>
-					<p>
-						Компания «Орбита Строй» открыла два строительных магазина в Дзержинске: на проспекте
-						Ленина и на улице Космонавтов – последняя площадка работает 7 дней в неделю, включая
-						воскресенье. Вам нужен надежный партнер, у которого можно в любое время приобрести все
-						необходимое для строительства, ремонта, а также инструменты для каменщиков, плотников,
-						отделочников и других мастеров? Обращайтесь в компанию «Орбита Строй» за следующими
-						товарами:
-					</p>
-					<ul>
-						<li>Строительные материалы</li>
-						<li>Материалы для отделки помещений</li>
-						<li>Электротовары</li>
-						<li>Сантехника</li>
-					</ul>
-					<p>У нас вы всегда можете купить стройматериалы через каталог.</p>
-				</div>
-			</div>
-		</div>
-	</div>
+<div class="bg-white overflow-hidden">
+  <div class="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+    <div class="hidden lg:block  absolute top-0 bottom-0 left-3/4 w-screen" />
+    <div
+      class="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none"
+    >
+      <div>
+        <h2
+          class="text-base text-indigo-600 font-semibold tracking-wide uppercase"
+        >
+          Строительная база в Нижнем Новгороде
+        </h2>
+        <h3
+          class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-indigo-900 sm:text-4xl"
+        >
+          Орбита-строй
+        </h3>
+      </div>
+    </div>
+    <div class="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+      <div class="relative lg:row-start-1 lg:col-start-2">
+        <svg
+          class="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
+          width="404"
+          height="384"
+          fill="none"
+          viewBox="0 0 404 384"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+              x="0"
+              y="0"
+              width="20"
+              height="20"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect
+                x="0"
+                y="0"
+                width="4"
+                height="4"
+                class="text-gray-200"
+                fill="currentColor"
+              />
+            </pattern>
+          </defs>
+          <rect
+            width="404"
+            height="384"
+            fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"
+          />
+        </svg>
+        <div class="relative text-base mx-auto max-w-prose lg:max-w-none">
+          <figure>
+            <div class="aspect-w-12 aspect-h-7 lg:aspect-none">
+              <img
+                class="rounded-lg shadow-lg object-cover object-center"
+                src="https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/pages/serv_4.jpg"
+                alt="Стройматериалы в нижнем новгороде"
+                width="1184"
+                height="1376"
+              />
+            </div>
+          </figure>
+        </div>
+      </div>
+      <div class="mt-8 lg:mt-0">
+        <div
+          class="mt-1 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1"
+        >
+          <p>
+            Приобрести стройматериалы в Нижнем Новгороде высокого качества по
+            конкурентным ценам можно в течение нескольких минут. База
+            строительных и отделочных материалов «Орбита-Строй» занимает площадь
+            более, чем 1 км2 и предлагает продукцию от таких производителей,
+            как:
+          </p>
+          <ul>
+            <li>Tarkett;</li>
+            <li>EK;</li>
+            <li>Ceresit;</li>
+            <li>Makroflex;</li>
+          </ul>
+          <p>
+            Строительная база имеет удобный подъезд автотранспорта, развитую
+            инфраструктуру и просторные выставочные павильоны. Строительные
+            магазины в Нижнем Новгороде предоставляют услуги по погрузке при
+            оптовой покупке на бесплатных условиях, также возможно вывезти
+            стройматериалы с доставкой собственным автотранспортом компании.
+          </p>
+          <p>
+            Строительный интернет-магазин подойдёт тем, кто ценит время и
+            качество. Выбрать товар заблаговременно оплатить покупку
+            стройматериалов в Нижнем Новгороде и заказать их доставку по
+            указанному адресу – дело нескольких минут. Наша стройбаза идёт
+            навстречу постоянным клиентам и предоставляет гибкие дисконтные
+            скидки на строительные материалы ы Нижнем Новгороде.
+          </p>
+          <p>
+            «Орбита-Строй» - строительный интернет магазин в Нижнем Новгороде
+            для тех, кто уважает качество материалов и труд. «Орбита-Строй» -
+            правильный выбор на пути к комфорту и надёжности.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
--- app/src/lib/components/header/hero/index.svelte
+++ app/src/lib/components/header/hero/index.svelte
@@ -5,10 +5,10 @@
 <div class="max-w-9xl mx-auto  px-4 sm:px-6 lg:px-8">
 	<div class="mx-auto  text-center">
 		<h2
-			class="py-6 text-3xl font-bold text-indigo-900 antialiased lowercase first-letter:uppercase sm:text-4xl lg:text-5xl"
+			class="py-6 text-3xl font-bold text-indigo-900 antialiased sm:text-4xl lg:text-5xl"
 		>
 			<!-- {$pageH1} -->
-			База строительных материалов
+			База стройматериалов "Орбита-Строй"
 		</h2>
 		<img
 			class="inset-0 mt-2 h-64 w-full object-cover"
