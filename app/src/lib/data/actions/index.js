9a749527aa0c408baca259c57ebee513c21d36b2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-12-22 10:02:11 +0300

    c

--- app/src/lib/data/actions/index.js
+++ app/src/lib/data/actions/index.js
@@ -102,7 +102,7 @@ const actions = [
 	{
 		id: '228',
 		action: 'Плита осб-3 2500*1250*9мм kronospan гост (влагостойкая)',
-		price: '495',
+		price: '478',
 		description: 'При заказе через сайт дополнительная скидка 5%',
 		link: '/product/plita-osb-3-250012509mm-kronospan-gost-vlagostoikaya',
 		img: 'https://lumen-image-bucket.s3.eu-central-1.amazonaws.com/images/ilejI7dCV3zr72h4VudvlNrQLKBgHsrYwKd1HoAL.jpeg'
