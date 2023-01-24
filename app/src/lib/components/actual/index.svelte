c10264c84786790c4191f14e1df1d06cdd155d27
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-24 08:32:12 +0300

    с

--- app/src/lib/components/actual/index.svelte
+++ app/src/lib/components/actual/index.svelte
@@ -33,7 +33,7 @@
 			{#each actions as { id, action, price, description, link, img }}
 				<div class="group rounded-md relative shadow-lg shadow-slate-400/50 p-3">
 					<div
-						class="h-56 p-8 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md  group-hover:opacity-75 lg:aspect-none "
+						class="h-56 p-8 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md  group-hover:opacity-75 lg:aspect-none transition ease-in-out hover:scale-104"
 					>
 						<img src={img} alt="" class=" my-2  object-contain " />
 					</div>
