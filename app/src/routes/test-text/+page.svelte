65c05da7309a3f9662eb541a502f6135afc58fc8
indexpro24@gmail.com <indexpro24@gmail.com>
2023-01-25 15:23:43 +0300

    c

---
+++ app/src/routes/test-text/+page.svelte
@@ -0,0 +1,19 @@
+<div class="mt-8 lg:mt-0">
+	<div
+		class="mt-1 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1"
+	>
+		<p>Параграф</p>
+		<h2>Н2</h2>
+		<span>Строка</span>
+		<ol>
+			<li>Нумерованный список</li>
+		</ol>
+		<span>Строка</span>
+		<ul>
+			<li>Ненумерованный список</li>
+		</ul>
+		<span>Строка</span>
+		<h2>Н2</h2>
+		<p>Параграф</p>
+	</div>
+</div>
