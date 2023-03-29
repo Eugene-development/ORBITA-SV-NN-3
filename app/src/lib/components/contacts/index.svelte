1fba10b5bd394999e7ab78e70709144ef855ca87
indexpro24@gmail.com <indexpro24@gmail.com>
2023-03-29 14:25:48 +0300

    c

--- app/src/lib/components/contacts/index.svelte
+++ app/src/lib/components/contacts/index.svelte
@@ -252,3 +252,16 @@
 		</div>
 	</div>
 </div>
+
+<div class="-mt-12 pb-8 bg-gray-50 mx-16 overflow-hidden sm:rounded-lg shadow-xl">
+	<div class="px-4 py-4 sm:p-6 ">
+		<iframe
+			src="https://yandex.ru/maps/?um=constructor%3A31271ddffe7286db3d1f91ca03ff5a49e0b590980e803a5447660c43360dc4bd&source=constructorLink"
+			width="1024"
+			height="600"
+			title="map"
+			frameborder="0"
+			class="mx-auto w-full"
+		/>
+	</div>
+</div>
