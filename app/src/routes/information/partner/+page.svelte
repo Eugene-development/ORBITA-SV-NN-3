8c1cfd53780385782a2775063fc4a750f8cdc3de
Eugene Che <indexpro24@gmail.com>
2023-06-12 19:49:53 +0300

    c

--- app/src/routes/information/partner/+page.svelte
+++ app/src/routes/information/partner/+page.svelte
@@ -122,7 +122,11 @@
 											d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
 										/>
 									</svg>
-									<a href="http://remont-stroynn.ru/" class="hover:text-gray-500" target="_blank"
+									<a
+										href="http://remont-stroynn.ru/"
+										class="hover:text-gray-500"
+										target="_blank"
+										rel="noreferrer"
 										>Перейти на сайт компании
 										<span class="sr-only">Site</span>
 									</a>
