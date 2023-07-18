b86c72b2caeffb47324a5af8044de78b44f08435
Eugene Che <indexpro24@gmail.com>
2023-07-18 06:29:23 +0300

    c

--- app/src/routes/product/[slug]/+page.svelte
+++ app/src/routes/product/[slug]/+page.svelte
@@ -105,7 +105,10 @@
 						{#if $visibleDescription}
 							<!-- TODO: переделать -->
 							{#if data.product.product_one.text}
-								<p class="mb-4 leading-relaxed ">{@html data.product.product_one.text[1]?.value}</p>
+								<p class="mb-4 leading-relaxed ">
+									{@html data.product.product_one.text[1]?.value ??
+										data.product.product_one.text[0]?.value}
+								</p>
 							{:else}
 								<p class="mb-4 leading-relaxed ">Описание товара отсутствует</p>
 							{/if}
